35fbce191bac3fc51c4104dfea8ff2f47bc66602
berkay yıldız <berkayyildiz504@hotmail.com>
2021-02-03 18:26:05 +0300

    Add project files.

---
+++ LibraryProject.MvcWebUI/Models/Context/LibraryContext.cs
@@ -0,0 +1,25 @@
+﻿using LibraryProject.MvcWebUI.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LibraryProject.MvcWebUI.Models.Context
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext()
+        {
+            Database.Connection.ConnectionString = "Server = (localdb)\\MSSQLLocalDB; Database = LibraryDb; Trusted_Connection = True;";
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasRequired<Category>(b => b.Category).WithMany(c => c.Books).HasForeignKey(b => b.CategoryId);
+        }
+    }
+}
